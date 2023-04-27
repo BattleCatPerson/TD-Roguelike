e8b1387f65803e09b2c06d267a2f9772b26040a5
BattleCatPerson <93836439+BattleCatPerson@users.noreply.github.com>
2023-04-27 07:46:33 -0700

    a

--- Assets/Scripts/CarvePath.cs
+++ Assets/Scripts/CarvePath.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class CarvePath : MonoBehaviour
 {
-    [SerializeField] List<Transform> rows;
+    [SerializeField] List<Transform> transforms;
     Transform[,] grid;
+    [SerializeField] Transform startPosition;
+    [SerializeField] Transform endPosition;
+
     void Start()
     {
-        grid = new Transform[rows.Count, rows.Count];
+        grid = new Transform[transforms.Count, transforms.Count];
+        int count = 0;
+        for (int i = 0; i < transforms.Count; i++)
+        {
+            for (int j = 0; j < transforms.Count; j++)
+            {
+                grid[i, j] = transforms[count];
+                count++;
+            }
+        }
     }
 
     void Update()
