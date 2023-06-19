baec644dfca630e2d17da0914a06675c2a8a1aad
BattleCatPerson <dankmarkers123@gmail.com>
2023-06-19 17:52:51 -0400

    new map

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -14,10 +14,17 @@ public class MapManager : MonoBehaviour
     [SerializeField] string battleScene;
     [SerializeField] List<float> startingResources;
     [SerializeField] float startingHealth;
+
+    public static int Rounds = -1;
+    [SerializeField] int rounds;
+    [SerializeField] List<Button> buttons;
+    [SerializeField] GameObject panel;
+    [SerializeField] int options;
     private void Awake()
     {
+        if (Rounds == -1) Rounds = rounds;
         instance = this;
-        if (HealthManager.health == - 1 && !started)
+        if (HealthManager.health == -1 && !started)
         {
             SpawnTower.resources = startingResources;
             HealthManager.health = startingHealth;
@@ -28,27 +35,36 @@ public class MapManager : MonoBehaviour
     void Start()
     {
 
-        if (row + 1 < rows.Count)
-        {
-            availableButtons.Add(rows[row + 1].buttons[col * 2]);
-            availableButtons.Add(rows[row + 1].buttons[col * 2 + 1]);
-        }
+        //if (row + 1 < rows.Count)
+        //{
+        //    availableButtons.Add(rows[row + 1].buttons[col * 2]);
+        //    availableButtons.Add(rows[row + 1].buttons[col * 2 + 1]);
+        //}
         
-        foreach (TreeRow row in rows)
+        //foreach (TreeRow row in rows)
+        //{
+        //    foreach (Button button in row.buttons)
+        //    {
+        //        if (!availableButtons.Contains(button)) button.interactable = false;
+        //    }
+        //}
+
+        if (Rounds > 0)
         {
-            foreach (Button button in row.buttons)
+            for (int i = 0; i < options; i++)
             {
-                if (!availableButtons.Contains(button)) button.interactable = false;
+                var b = buttons[Random.Range(0, buttons.Count)];
+                buttons.Remove(b);
+                Instantiate(b, panel.transform);
             }
         }
-
-
     }
     public void LoadBattleScene(Button b)
     {
-        int index = rows[row + 1].buttons.IndexOf(b);
-        col = index;
-        row++;
+        //int index = rows[row + 1].buttons.IndexOf(b);
+        //col = index;
+        //row++;
+        Rounds--;
         SceneManager.LoadScene(battleScene);
     }
 }
